991a5bb476c5a31f12c4d47123807e2f8cfd0803
LeaClover <plesen_14@mail.ru>
2023-04-01 16:38:06 +0300

    add file service project file

---
+++ homework3/src/main/java/com/gb/java/service/FileService.java
@@ -0,0 +1,21 @@
+﻿package com.gb.java.service;
+import java.io.FileWriter;
+import java.io.IOException;
+import com.gb.java.data.User;
+
+public class FileService {
+    
+    public void writeFileCSV(User user) {
+        try (FileWriter fw = new FileWriter("src/main/java/com/gb/java/files/" + user.getLastname() + ".csv", true)) {
+            fw.append(String.format("<%s>", user.getLastname()));
+            fw.append(String.format("<%s>", user.getFirstname()));
+            fw.append(String.format("<%s>", user.getPatronymic()));
+            fw.append(String.format("<%s>", user.getBirthday()));
+            fw.append(String.format("<%s>", user.getPhone()));
+            fw.append(String.format("<%s>\n", user.getGender()));
+            fw.close();
+        } catch (IOException e) {
+            new IOException("ERROR: Что-то пошло не так! Невозможно записать данные в файл!");
+        }
+    }
+}
