474731f33ae7a5723bac7a37564e667d29898756
LeaClover <plesen_14@mail.ru>
2023-04-01 16:38:28 +0300

    add user view project file

---
+++ homework3/src/main/java/com/gb/java/view/UserView.java
@@ -0,0 +1,12 @@
+﻿package com.gb.java.view;
+import java.util.logging.Logger;
+import com.gb.java.data.User;
+
+public class UserView {
+    
+    Logger log = Logger.getAnonymousLogger();
+
+    public void sendOnController(User user) {
+        log.info("Добавлен новый пользователь: " + user.toString());
+    }
+}
