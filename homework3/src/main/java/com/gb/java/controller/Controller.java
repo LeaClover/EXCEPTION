5d45f6ecb7f8593a0442ee1e0cae227384cae5bf
LeaClover <plesen_14@mail.ru>
2023-04-01 16:37:24 +0300

    add controller project file

---
+++ homework3/src/main/java/com/gb/java/controller/Controller.java
@@ -0,0 +1,15 @@
+﻿package com.gb.java.controller;
+import com.gb.java.data.User;
+import com.gb.java.service.FileService;
+import com.gb.java.view.UserView;
+
+public class Controller {
+
+    private final UserView uv = new UserView();
+    private final FileService fs = new FileService();
+
+    public void addUser(User user) {
+        fs.writeFileCSV(user);
+        uv.sendOnController(user);
+    }
+}
